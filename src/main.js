074afbf5e31149bbb54f1b10944fcc70ed206b0e
Artur Voll <artur.voll@gmail.com>
2020-09-22 19:35:36 +0200

    vue2 init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 import store from './store'
+import vuetify from './plugins/vuetify';
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css';
 
-createApp(App).use(store).mount('#app')
+Vue.config.productionTip = false;
+Vue.component('v-select', vSelect.vSelect)
+
+new Vue({
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
