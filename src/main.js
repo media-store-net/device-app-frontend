8831e5621c2331481e4773c06045c2cf6cd37896
Artur Voll <artur.voll@gmail.com>
2020-09-22 20:42:24 +0200

    fixed e2e test

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-import store from './store'
+import store from './store';
 import vuetify from './plugins/vuetify';
 import vSelect from 'vue-select'
 import 'vue-select/dist/vue-select.css';
 
 Vue.config.productionTip = false;
-Vue.component('v-select', vSelect.vSelect)
+Vue.component('v-select', vSelect.vSelect);
 
 new Vue({
   store,
