90abe080eba7ae0c7701cc5aecafdbc5d0ff40fc
Artur Voll <artur.voll@gmail.com>
2020-11-05 17:55:11 +0100

    defined new theme colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,6 +5,17 @@ import "vuetify/dist/vuetify.min.css"
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    light: true,
+    themes: {
+      light: {
+        primary: '#2258f9',
+        secondary: '#b0bec5',
+        anchor: '#36477A',
+        accent: '#112B7A',
+      },
+    },
+  },
   icons: {
     iconfont: "mdiSvg", // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
   },
