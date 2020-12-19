7eecb2b89a31e14150ba836cf994debada1a4425
Антон Берестов <8berestov8@gmail.com>
2020-12-19 21:19:57 +0400

    added component ClouBtn & HarddiskBtn & PlusBoxBtn

---
+++ src/components/UI/CloudBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn
+    icon
+  >
+    <v-icon>{{ addCloudBtn }} </v-icon>
+  </v-btn>
+</template>
+
+<script>
+import { mdiCloudDownload } from '@mdi/js';
+
+export default {
+  name: 'AddCloudBtn',
+  data() {
+    return {
+     addCloudBtn: mdiCloudDownload,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
