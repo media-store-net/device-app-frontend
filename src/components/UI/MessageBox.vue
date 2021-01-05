2d3f043fc2678dcd399321d1e308deda8b2c6984
Artur Voll <artur.voll@gmail.com>
2021-01-05 13:50:32 +0100

    fixed MessageBox.vue with props

--- src/components/UI/MessageBox.vue
+++ src/components/UI/MessageBox.vue
@@ -1,15 +1,28 @@
-<template>
-  <v-alert
-    dense
-    type="error"
-  >
-    I'm a dense alert with a <strong>type</strong> of info
-  </v-alert>
+<template xmlns:slot="http://www.w3.org/1999/html">
+  <v-app class="d-flex">
+    <v-alert
+        dismissible
+        :close-icon="CloseBtn"
+        :type="type"
+    >
+      {{ message }}
+    </v-alert>
+  </v-app>
 </template>
 
 <script>
+import CloseBtn from "@/components/UI/CloseBtn";
 export default {
-  name: "MessageBox"
+  name: "MessageBox",
+  props: {
+    type: {
+      type: String
+    },
+    message: {
+      type: String
+    }
+  },
+  emits: ['close-message']
 };
 </script>
 
