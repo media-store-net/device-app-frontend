447c9bdb175325419cc7f924b57fb364c859635a
Artur Voll <artur.voll@gmail.com>
2021-02-12 20:17:15 +0100

    Convert data to Upload in devices.post && devices.put

--- src/api/api.js
+++ src/api/api.js
@@ -1,27 +1,27 @@
 import API from "./index";
 
 export default {
-  setAuthHeader(token) {
-    API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  },
-  removeAuthHeader() {
-    delete API.defaults.headers.common["Authorization"];
-  },
-  companies: {
-    get() {
-      let params = new URLSearchParams();
-      return API.get("/companies", params);
-    },
-    post(data) {
-      return API.post("/companies", data);
-    },
-    put(data) {
-      return API.put('/companies', data)
-    },
-    delete(id) {
-      return API.put('/companies', id)
-    }
-  },
+	setAuthHeader(token) {
+		API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+	},
+	removeAuthHeader() {
+		delete API.defaults.headers.common["Authorization"];
+	},
+	companies: {
+		get() {
+			let params = new URLSearchParams();
+			return API.get("/companies", params);
+		},
+		post(data) {
+			return API.post("/companies", data);
+		},
+		put(data) {
+			return API.put('/companies', data)
+		},
+		delete(id) {
+			return API.put('/companies', id)
+		}
+	},
 	upload: {
 		get() {
 			let params = new URLSearchParams();
@@ -52,56 +52,62 @@ export default {
 			return API.put('/files/' + id)
 		}
 	},
-  devices: {
-    get() {
-      let params = new URLSearchParams();
-      return API.get("/devices", params);
-    },
-	  post(device) {
-		  return API.post("/devices", device);
-	  },
-	  put(device) {
-		  return API.put(`/devices/${device.id}`, device)
-	  },
-	  delete(id) {
-		  return API.delete('/devices/' + id)
-	  }
-  },
+	devices: {
+		get() {
+			let params = new URLSearchParams();
+			return API.get("/devices", params);
+		},
+		post(device) {
+			// Convert Objects to only Ids
+			device.part = [device.part.id];
+			device.companie = [device.companie.id];
+			return API.post("/devices", device);
+		},
+		put(device) {
+			// Convert Objects to only Ids
+			device.part = [device.part.id];
+			device.companie = [device.companie.id];
+			return API.put(`/devices/${device.id}`, device)
+		},
+		delete(id) {
+			return API.delete('/devices/' + id)
+		}
+	},
 
-  doctypes: {
-    get() {
-      let params = new URLSearchParams();
-      return API.get("/doctypes ", params);
-    },
-	  post(data) {
-		  return API.post("/doctypes", data);
-	  },
-	  put(data) {
-		  return API.put('/doctypes', data)
-	  },
-	  delete(id) {
-		  return API.put('/doctypes', id)
-	  }
-  },
-  parts: {
-    get() {
-      let params = new URLSearchParams();
-      return API.get("/parts", params);
-    },
-	  post(data) {
-		  return API.post("/parts", data);
-	  },
-	  put(data) {
-		  return API.put('/parts', data)
-	  },
-	  delete(id) {
-		  return API.put('/parts', id)
-	  }
-  },
-  deviceLogin(params) {
-    return API.post("/devices/customer-login", params);
-  },
-  userLogin(params) {
-    return API.post("/auth/local", params);
-  }
+	doctypes: {
+		get() {
+			let params = new URLSearchParams();
+			return API.get("/doctypes ", params);
+		},
+		post(data) {
+			return API.post("/doctypes", data);
+		},
+		put(data) {
+			return API.put('/doctypes', data)
+		},
+		delete(id) {
+			return API.put('/doctypes', id)
+		}
+	},
+	parts: {
+		get() {
+			let params = new URLSearchParams();
+			return API.get("/parts", params);
+		},
+		post(data) {
+			return API.post("/parts", data);
+		},
+		put(data) {
+			return API.put('/parts', data)
+		},
+		delete(id) {
+			return API.put('/parts', id)
+		}
+	},
+	deviceLogin(params) {
+		return API.post("/devices/customer-login", params);
+	},
+	userLogin(params) {
+		return API.post("/auth/local", params);
+	}
 };
